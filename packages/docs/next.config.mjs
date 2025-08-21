eb51dbdbc032a4736bf609f673ff5dd573ef1857
Wei Zhang <kweizh@tabbyml.com>
2025-08-22 00:39:19 +0800

    fix(doc-site): fix page base uri

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -2,7 +2,7 @@ import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/pochi'
 
 /** @type {import('next').NextConfig} */
 const config = {
