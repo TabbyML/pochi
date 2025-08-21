06b7025b7b2a73bfe77b778195915b9480fae03e
Wei Zhang <kweizh@tabbyml.com>
2025-08-22 00:46:26 +0800

    fix(ci): choose base uri only in ci

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -2,7 +2,7 @@ import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/pochi'
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
 
 /** @type {import('next').NextConfig} */
 const config = {
