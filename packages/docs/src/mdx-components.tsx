d5b20e4e7f2be654cff07c5066803f60b56c23f9
Meng Zhang <meng@tabbyml.com>
2025-08-24 21:50:05 +0800

    update

--- packages/docs/src/mdx-components.tsx
+++ packages/docs/src/mdx-components.tsx
@@ -2,11 +2,13 @@ import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import ExportedImage from "next-image-export-optimizer";
 
+const basePath = process.env.__NEXT_ROUTER_BASEPATH
+
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    img: (props) => <ExportedImage {...props} />,
+    img: (props) => <ExportedImage {...props} basePath={basePath} />,
     ...components,
   };
 }
