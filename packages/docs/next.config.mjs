1a6154181fed6ba5aca8d7bee5683b24ce3b6e2d
Meng Zhang <meng@tabbyml.com>
2025-08-24 21:38:53 +0800

    fix: update imageSizes and deviceSizes for better image handling

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -13,7 +13,8 @@ const config = {
   trailingSlash: true, // generate index.html
   images: {
     loader: "custom",
-    imageSizes: [320, 640, 1200],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   transpilePackages: ["next-image-export-optimizer"],
   env: {
