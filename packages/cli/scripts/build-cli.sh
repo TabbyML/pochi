1435e992c1c9878bcb9b88cbc0ed22f3a69af73f
Wei Zhang <kweizh@tabbyml.com>
2025-09-08 01:12:34 +0800

    refactor(cli): use dispatcher as cli entrypoint and release cli itself to npm

--- packages/cli/scripts/build-cli.sh
+++ packages/cli/scripts/build-cli.sh
@@ -2,22 +2,17 @@
 set -ex
 
 build_js() {
-        bun build src/cli.ts \
+        bun build src/cli.bun.ts \
                 --external lightningcss \
                 --target node \
                 --outdir ./dist \
                 --asset-naming="[name].[ext]" \
                 --sourcemap=inline \
                 "$@"
-
-        # we use bun shebang in cli.ts to utilize bun link
-        # change to node for npm release
-        sed -i 's|#!/usr/bin/env bun|#!/usr/bin/env node|g' ./dist/cli.js
-        chmod +x ./dist/cli.js
 }
 
 build_exe() {
-        bun build src/cli.ts --banner='import * as undici from "undici";' \
+        bun build src/cli.bun.ts --banner='import * as undici from "undici";' \
                 --asset-naming="[name].[ext]" \
                 --external lightningcss \
                 --compile \
