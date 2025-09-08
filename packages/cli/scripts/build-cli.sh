f651b6e9135f74a0d581615bacb08d8266b2475c
Wei Zhang <kweizh@tabbyml.com>
2025-09-08 14:18:37 +0800

    chore: add dispatcher when build nodejs and use bun publish

--- packages/cli/scripts/build-cli.sh
+++ packages/cli/scripts/build-cli.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 set -ex
 
+# we upload the released file to npm and run using node,
+# we use bun to utilize local dev,
+# so add this dispatcher if run bun locally
 build_js() {
-        bun build src/cli.bun.ts \
+        bun build src/cli.ts \
+                --banner 'const isBun = !!process.versions.bun;
+if (isBun) {
+  await import("../src/cli.ts");
+  process.exit(0);
+}
+' \
                 --external lightningcss \
                 --target node \
                 --outdir ./dist \
                 --asset-naming="[name].[ext]" \
                 --sourcemap=inline \
                 "$@"
+
+        sed -i '1s|^.*$|#!/usr/bin/env node|' ./dist/cli.js
 }
 
 build_exe() {
-        bun build src/cli.bun.ts --banner='import * as undici from "undici";' \
+        bun build src/cli.ts --banner='import * as undici from "undici";' \
                 --asset-naming="[name].[ext]" \
                 --external lightningcss \
                 --compile \
