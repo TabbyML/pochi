1ad76bc2a324a8683118c66711ea4f25a9e4b457
Meng Zhang <meng@tabbyml.com>
2025-09-09 16:35:25 +0800

    update

--- packages/vscode-webui/src/components/message/code-block.tsx
+++ packages/vscode-webui/src/components/message/code-block.tsx
@@ -35,15 +35,6 @@ export interface CodeBlockProps {
   isMinimalView?: boolean;
 }
 
-export const generateRandomString = (length: number, lowercase = false) => {
-  const chars = "ABCDEFGHJKLMNPQRSTUVWXY3456789"; // excluding similar looking characters like Z, 2, I, 1, O, 0
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return lowercase ? result.toLowerCase() : result;
-};
-
 interface MenuButtonProps {
   onClick: () => void;
   children: React.ReactNode;
