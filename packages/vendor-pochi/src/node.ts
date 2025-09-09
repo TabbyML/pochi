d2a96c5485c6296139cd462a80006580dce2b0d2
Meng Zhang <meng@tabbyml.com>
2025-09-09 19:43:51 +0800

    feat(vscode): integrate vendor configuration updates and remove token storage class

--- packages/vendor-pochi/src/node.ts
+++ packages/vendor-pochi/src/node.ts
@@ -1,12 +1,29 @@
-import { pochiConfig } from "@getpochi/common/configuration";
+import {
+  pochiConfig,
+  updateVendorConfig,
+} from "@getpochi/common/configuration";
 import { registerVendor } from "@getpochi/common/vendor";
 import type { PochiCredentials } from "./types";
 import { Pochi } from "./vendor";
 
 registerVendor(new Pochi());
 
+export type { PochiCredentials } from "./types";
+export { createAuthClient } from "./vendor";
+
 export function getPochiCredentials() {
   return pochiConfig.value.vendors?.pochi?.credentials as
     | PochiCredentials
     | undefined;
+}
+
+export function updatePochiCredentials(credentials: PochiCredentials | null) {
+  updateVendorConfig(
+    "pochi",
+    credentials
+      ? {
+          credentials,
+        }
+      : null,
+  );
 }
