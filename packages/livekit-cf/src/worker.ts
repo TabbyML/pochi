b60ee11df8c640db4d4132e0c9c234508a90f085
Meng Zhang <meng@tabbyml.com>
2025-09-17 10:19:54 -0700

    update

--- packages/livekit-cf/src/worker.ts
+++ packages/livekit-cf/src/worker.ts
@@ -1,37 +1,8 @@
 import type { CfTypes } from "@livestore/sync-cf/cf-worker";
-import * as SyncBackend from "@livestore/sync-cf/cf-worker";
-import { DoSqlD1 } from "./lib/do-sql-d1";
 import { fetch } from "./router";
 import type { Env } from "./types";
 
-let signalKeepAliveClientDo: ((storeId: string) => Promise<void>) | undefined =
-  undefined;
-
-export class SyncBackendDO extends SyncBackend.makeDurableObject({
-  onPush: async (_message, { storeId }) => {
-    await signalKeepAliveClientDo?.(storeId);
-  },
-}) {
-  constructor(
-    state: CfTypes.DurableObjectState,
-    private readonly doEnv: Env,
-  ) {
-    super(state, {
-      ...doEnv,
-      DB: new DoSqlD1(state.storage.sql),
-    });
-
-    signalKeepAliveClientDo = this.signalKeepAliveClientDo.bind(this);
-  }
-
-  async signalKeepAliveClientDo(storeId: string) {
-    const id = this.doEnv.CLIENT_DO.idFromName(storeId);
-    const stub = this.doEnv.CLIENT_DO.get(id);
-    await stub.signalKeepAlive(storeId);
-  }
-}
-
-// Scoped by storeId
+export { SyncBackendDO } from "./sync";
 export { LiveStoreClientDO } from "./client";
 
 export default {
