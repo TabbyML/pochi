035531a362346df855a7841ddaa48b7127f7842e
Allen <zhallen12261@gmail.com>
2025-09-21 19:13:51 -0400

    refactor(cli): reorder imports for better readability

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -23,8 +23,8 @@ import * as commander from "commander";
 import packageJson from "../package.json";
 import { registerAuthCommand } from "./auth";
 
-import { initializeShellCompletion } from "./completion/tree";
 import { registerCompletionCommand } from "./completion";
+import { initializeShellCompletion } from "./completion/tree";
 import { findRipgrep } from "./lib/find-ripgrep";
 import { loadAgents } from "./lib/load-agents";
 import { shutdownStoreAndExit } from "./lib/store-utils";
