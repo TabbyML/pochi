038848ddc18dc982308e37dd0ff6af4ed5c80ffa
Allen <zhallen12261@gmail.com>
2025-10-07 10:12:18 -0400

    fix(cli): correct completion script templates and fix spacing in guide
    Update completion script templates to use 'pochi' instead of placeholder '{pkgname}'
    Fix spacing in the installation guide message

--- packages/cli/src/completion/cmd.ts
+++ packages/cli/src/completion/cmd.ts
@@ -10,7 +10,7 @@ export function registerCompletionCommand(program: CommandUnknownOpts) {
     .option("--fish", "Generate fish completion script")
     .action((options) => {
       const installDir = "~/.pochi";
-      const guide = `Copy below script and save it as ${installDir}/.pochi-completion.sh .Then reload your terminal.\n`;
+      const guide = `Copy below script and save it as ${installDir}/.pochi-completion.sh . Then reload your terminal.\n`;
       if (options.bash) {
         console.log(guide);
         console.log(bashCompletionCommand);
@@ -34,26 +34,24 @@ export function registerCompletionCommand(program: CommandUnknownOpts) {
 }
 
 const zshCompletionCommand = `
-###-begin-{pkgname}-completion-###
+###-begin-pochi-completion-###
 if type compdef &>/dev/null; then
-  _{pkgname}_completion () {
+  _pochi_completion() {
     local reply
     local si=$IFS
-
-    IFS=$'\n' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" {completer} completion -- "\${words[@]}"))
+    IFS=$'\n' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" pochi completion -- "\${words[@]}"))
     IFS=$si
-
     _describe 'values' reply
   }
-  compdef _{pkgname}_completion {pkgname}
+  compdef _pochi_completion pochi
 fi
-###-end-{pkgname}-completion-###
+###-end-pochi-completion-###
 `;
 
 const bashCompletionCommand = `
-###-begin-{pkgname}-completion-###
+###-begin-pochi-completion-###
 if type complete &>/dev/null; then
-  _{pkgname}_completion () {
+  _pochi_completion () {
     local words cword
     if type _get_comp_words_by_ref &>/dev/null; then
       _get_comp_words_by_ref -n = -n @ -n : -w words -i cword
@@ -66,32 +64,32 @@ if type complete &>/dev/null; then
     IFS=$'\n' COMPREPLY=($(COMP_CWORD="$cword" \
                            COMP_LINE="$COMP_LINE" \
                            COMP_POINT="$COMP_POINT" \
-                           {completer} completion -- "\${words[@]}" \
+                           pochi completion -- "\${words[@]}" \
                            2>/dev/null)) || return $?
     IFS="$si"
     if type __ltrim_colon_completions &>/dev/null; then
       __ltrim_colon_completions "\${words[cword]}"
     fi
   }
-  complete -o default -F _{pkgname}_completion {pkgname}
+  complete -o default -F _pochi_completion pochi
 fi
-###-end-{pkgname}-completion-###
+###-end-pochi-completion-###
 `;
 
 const fishCompletionCommand = `
-###-begin-{pkgname}-completion-###
-function _{pkgname}_completion
+###-begin-pochi-completion-###
+function _pochi_completion
   set cmd (commandline -o)
   set cursor (commandline -C)
   set words (node -pe "'$cmd'.split(' ').length")
 
-  set completions (eval env DEBUG=\"" \"" COMP_CWORD=\""$words\"" COMP_LINE=\""$cmd \"" COMP_POINT=\""$cursor\"" {completer} completion -- $cmd)
+  set completions (eval env DEBUG="" COMP_CWORD="$words" COMP_LINE="$cmd " COMP_POINT="$cursor" pochi completion -- $cmd)
 
   for completion in $completions
     echo -e $completion
   end
 end
 
-complete -f -d '{pkgname}' -c {pkgname} -a "(eval _{pkgname}_completion)"
-###-end-{pkgname}-completion-###
+complete -f -d 'pochi' -c pochi -a "(eval _pochi_completion)"
+###-end-pochi-completion-###
 `;
