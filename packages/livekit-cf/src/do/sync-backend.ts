a78cf534ec8889cfcc930099526fd2a1fda6660d
Meng Zhang <meng@tabbyml.com>
2025-10-16 01:33:41 -0700

    feat: update server base URL for development environment

--- packages/livekit-cf/src/do/sync-backend.ts
+++ packages/livekit-cf/src/do/sync-backend.ts
@@ -1,10 +1,21 @@
 import { env } from "cloudflare:workers";
+import { verifyStoreId } from "@/lib/jwt";
 import * as SyncBackend from "@livestore/sync-cf/cf-worker";
 
 export class SyncBackendDO extends SyncBackend.makeDurableObject({
-  onPush: async (_message, { storeId }) => {
+  onPush: async (_message, { storeId, payload }) => {
+    // Once connection is confirmed, we extend trust of JWT by 7 days as clock tolerance.
+    if (!(await verifyStoreId(payload, storeId))) {
+      throw new Error("Unauthorized");
+    }
     const id = env.CLIENT_DO.idFromName(storeId);
     const stub = env.CLIENT_DO.get(id);
     await stub.signalKeepAlive(storeId);
+  },
+  onPull: async (_message, { storeId, payload }) => {
+    // Once connection is confirmed, we extend trust of JWT by 7 days as clock tolerance.
+    if (!(await verifyStoreId(payload, storeId))) {
+      throw new Error("Unauthorized");
+    }
   },
 }) {}
