7c6494b2d0afbe08238e8a8c96860b811aa05a35
swellee <svtt@163.com>
2025-12-09 22:09:12 +0800

    fix lint error

--- packages/vscode-webui/src/routes/index.tsx
+++ packages/vscode-webui/src/routes/index.tsx
@@ -92,6 +92,7 @@ function Tasks() {
 
   // 重置累积的 tasks 当 cwd 变化时
   useEffect(() => {
+    console.log("cwd changed, resetting accumulated tasks");
     setAccumulatedTasks([]);
     setCurrentPage(1);
   }, [cwd]);
