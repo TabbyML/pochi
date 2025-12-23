49ec20b3f126b08dffed535c9796612bd06fe972
swellee <svtt@163.com>
2025-12-23 18:15:08 +0800

    revert viteconfig of alias

--- packages/vscode-webui/vite.config.js
+++ packages/vscode-webui/vite.config.js
@@ -147,12 +147,4 @@ export default defineConfig({
       protocol: "ws",
     },
   },
-  resolve: {
-    alias: {
-      "#use-paginated-tasks": resolve(
-        __dirname,
-        "./src/lib/hooks/use-paginated-tasks",
-      ),
-    },
-  },
 });
