f61da10c8a5f79f4f236f5401aec7361e7f578ac
Meng Zhang <meng@tabbyml.com>
2026-01-14 12:09:44 +0800

    update

--- packages/vscode-webui/src/lib/use-default-store.tsx
+++ packages/vscode-webui/src/lib/use-default-store.tsx
@@ -6,6 +6,7 @@ import {
 import LiveStoreSharedWorker from "@livestore/adapter-web/shared-worker?sharedworker&inline";
 import type { Store } from "@livestore/livestore";
 import { type ReactApi, storeOptions, useStore } from "@livestore/react";
+import type React from "react";
 import { createContext, useContext } from "react";
 import LiveStoreWorker from "../livestore.default.worker.ts?worker&inline";
 
@@ -17,64 +18,49 @@ const adapter = makePersistedAdapter({
 
 const inMemoryAdapter = makeInMemoryAdapter();
 
-function defaultStoreOptions(
-  storeId: "share-page" | string,
-  jwt: string | null,
-) {
-  if (storeId === "share-page") {
-  }
+type DefaultStoreOptionsProviderProps = {
+  children: React.ReactNode;
+} & (
+  | {
+      type: "share-page";
+    }
+  | {
+      type?: "vscode";
+      storeId: string;
+      jwt: string | null;
+    }
+);
 
+function defaultStoreOptions(props: DefaultStoreOptionsProviderProps) {
+  if (props.type === "share-page") {
+    return storeOptions({
+      storeId: "share-page",
+      schema: catalog.schema,
+      adapter: inMemoryAdapter,
+      disableDevtools: true,
+    });
+  }
   return storeOptions({
-    storeId,
+    storeId: props.storeId,
     schema: catalog.schema,
     adapter,
-    syncPayload: { jwt },
+    syncPayload: { jwt: props.jwt },
     disableDevtools: true,
   });
 }
-
 const DefaultStoreOptionsContext = createContext<ReturnType<
   typeof defaultStoreOptions
 > | null>(null);
 
-export function DefaultStoreOptionsProvider(
-  props: {
-    children: React.ReactNode;
-  } & (
-    | {
-        type: "share-page";
-      }
-    | {
-        type?: "vscode";
-        storeId: string;
-        jwt: string | null;
-      }
-  ),
-) {
-  const options = (() => {
-    if (props.type === "share-page") {
-      return storeOptions({
-        storeId: "share-page",
-        schema: catalog.schema,
-        adapter: inMemoryAdapter,
-        disableDevtools: true,
-      });
-    }
-    return storeOptions({
-      storeId: props.storeId,
-      schema: catalog.schema,
-      adapter,
-      syncPayload: { jwt: props.jwt },
-      disableDevtools: true,
-    });
-  })();
-
+export const DefaultStoreOptionsProvider: React.FC<
+  DefaultStoreOptionsProviderProps
+> = (props) => {
   return (
-    <DefaultStoreOptionsContext.Provider value={options}>
+    <DefaultStoreOptionsContext.Provider value={defaultStoreOptions(props)}>
       {props.children}
     </DefaultStoreOptionsContext.Provider>
   );
-}
+};
 
 export function useDefaultStore(): Store<typeof catalog.schema> & ReactApi {
   const storeOptions = useContext(DefaultStoreOptionsContext);
